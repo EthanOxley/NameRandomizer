0ebbd6afa1f802d2483f9633db9e833d951fdaac
Ethan Oxley <ethanoxley133@gmail.com>
2020-10-21 00:10:53 -0500

    Initial Commit

---
+++ herorando.js
@@ -0,0 +1,14 @@
+// Start of Pseudocode
+
+// Define three variables set to values with arrays for names, powers, and adjectives
+
+// Generate a random value to determine displayed items
+
+// End of Pseudocode
+
+var heroName = ["Iron Man", "Captain America", "Spiderman", "Wolverine", "Thor"];
+
+var heroPower = ["Super Speed", "Regeneration", "Teleportation", "Super Strength", "X-Ray Vision"];
+
+var heroAdjective = ["Amazing", "Generous", "Powerful", "Cunning", "Resourceful"];
+
